61cd2298ed69a2fc5e68a96108b743a8ff0099ce
Mohamed9o0 <vvtbcfcccgggfd@gmail.com>
2020-09-27 16:07:31 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzU3OTMyOTc3NDk2OTE1OTg4.X2nmFw.rXhSd_W_5JLoTJMDaT2kZjLHc1k', /// توكن البوت
+    YT_API_KEY: 'api ', /// مش مهم
+    prefix: '1', // هنا برفكس او مقدمة البوت
+    devs: ['438006664558280725'] //// ايدي حسابك
+}
